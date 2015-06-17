0de660c6ea44308f13989831b796446e08676187
Christopher Abad <aempirei@256.bz>
2015-06-17 05:24:57 -0700

    .

--- elccs.ino
+++ elccs.ino
@@ -14,192 +14,222 @@
 
 struct linebuf {
 
-  static const uint8_t line_max_sz = 80;
-  static const char EOL = '\n';
-  static const char NUL = '\0';
+	static const uint8_t line_max_sz = 80;
+	static const char EOL = '\n';
+	static const char NUL = '\0';
 
-  char line[line_max_sz + 1];
-  uint8_t line_sz;
-  bool locked;
+	char line[line_max_sz + 1];
+	uint8_t line_sz;
+	bool locked;
 
-  linebuf() :
-    line_sz(0), locked(false) {
-  }
+	linebuf() :
+		line_sz(0), locked(false) {
+		}
 
-  bool empty() const {
-    return line_sz == 0;
-  }
+	bool empty() const {
+		return line_sz == 0;
+	}
 
-  int lastch() const {
-    return empty() ? -1 : line[line_sz - 1];
-  }
+	int lastch() const {
+		return empty() ? -1 : line[line_sz - 1];
+	}
 
-  bool full() const {
-    return line_sz == line_max_sz;
-  }
+	bool full() const {
+		return line_sz == line_max_sz;
+	}
 
-  bool is_ready() const {
-    return locked;
-  }
+	bool is_ready() const {
+		return locked;
+	}
 
-  bool match(const char *s) const {
-    return is_ready() ? (strcasecmp(line, s) == 0) : false;
-  }
+	bool match(const char *s) const {
+		return is_ready() ? (strcasecmp(line, s) == 0) : false;
+	}
 
-  void update() {
+	void update() {
 
-    while (not is_ready() and Serial.available() > 0) {
+		while (not is_ready() and Serial.available() > 0) {
 
-      line[line_sz++] = Serial.read();
+			line[line_sz++] = Serial.read();
 
-      if (lastch() == EOL) {
+			if (lastch() == EOL) {
 
-        line[--line_sz] = NUL;
-        locked = true;
+				line[--line_sz] = NUL;
+				locked = true;
 
-      }
-      else if (full()) {
+			}
+			else if (full()) {
 
-        line[line_sz] = NUL;
-        locked = true;
-      }
-    }
-  }
+				line[line_sz] = NUL;
+				locked = true;
+			}
+		}
+	}
 
-  void clear() {
-    line_sz = 0;
-    locked = false;
-  }
+	void clear() {
+		line_sz = 0;
+		locked = false;
+	}
 };
 
 struct port {
 
-  uint8_t pin;
-  uint16_t mode;
+	uint8_t pin;
+	uint16_t mode;
 
-  bool analog;
+	bool analog;
 
-  int s0;
-  int s1;
+	int s0;
+	int s1;
 
-  unsigned long m0;
-  unsigned long m1;
-  long dm0;
+	unsigned long m0;
+	unsigned long m1;
+	long dm0;
 
-  bool active;
+	bool active;
 
-  int ds() {
-    return s0 - s1;
-  }
+	int ds() {
+		return s0 - s1;
+	}
 
-  template <typename T, typename U> void term(const char *key, T x, U b) {
-    Serial.print(" ");
-    Serial.print(key);
-    Serial.print("=");
-    Serial.print(x, b);
-  }
+	template <typename T, typename U> void term(const char *key, T x, U b) {
+		Serial.print(" ");
+		Serial.print(key);
+		Serial.print("=");
+		Serial.print(x, b);
+	}
 
-  port(int my_pin, int my_mode, bool my_analog = false, int s = LOW) : pin(my_pin), mode(my_mode), analog(my_analog), active(true) {
+	port(int my_pin, int my_mode, bool my_analog = false, int s = LOW) : pin(my_pin), mode(my_mode), analog(my_analog), active(true) {
 
-    pinMode(pin, mode);
+		pinMode(pin, mode);
 
-    m1 = millis();
+		m1 = millis();
 
-    if (mode == OUTPUT) {
-      s0 = s;
-      write(s);
-    } else {
-      s0 = get();
-      read();
-    }
-  }
+		if (mode == OUTPUT) {
+			s0 = s;
+			write(s);
+		} else {
+			s0 = get();
+			read();
+		}
+	}
 
-  void status(const char *msg = NULL) {
+	void status(const char *msg = NULL) {
 
-    Serial.print(";");
+		Serial.print(";");
 
-    term("PIN"   , pin   , DEC);
-    term("ACTIVE", active, DEC);
-    term("S"     , s0    , DEC);
-    term("DS"    , ds()  , DEC);
-    term("DM"    , dm()  , DEC);
+		term("PIN"   , pin   , DEC);
+		term("ACTIVE", active, DEC);
+		term("S"     , s0    , DEC);
+		term("DS"    , ds()  , DEC);
+		term("DM"    , dm()  , DEC);
 
-    if (msg != NULL) {
-      Serial.print(" :");
-      Serial.print(msg);
-    }
+		if (msg != NULL) {
+			Serial.print(" :");
+			Serial.print(msg);
+		}
 
-    Serial.println("");
-  }
+		Serial.println("");
+	}
 
-  int get() {
-    return (analog ? analogRead : digitalRead)(pin);
-  }
+	int get() {
+		return (analog ? analogRead : digitalRead)(pin);
+	}
 
-  int set(int s) {
-    if (analog)
-      analogWrite(pin, s);
-    else
-      digitalWrite(pin, s);
-    return s;
-  }
+	int set(int s) {
+		if (analog)
+			analogWrite(pin, s);
+		else
+			digitalWrite(pin, s);
+		return s;
+	}
 
-  int dm() {
-    return ds() == 0 ? m0 - m1 : dm0;
-  }
+	int dm() {
+		return ds() == 0 ? m0 - m1 : dm0;
+	}
 
-  int push(int s) {
+	int push(int s) {
 
-    s1 = s0;
-    s0 = s;
+		s1 = s0;
+		s0 = s;
 
-    m0 = millis();
+		m0 = millis();
 
-    if (ds() != 0) {
-      dm0 = m0 - m1;
-      m1 = m0;
-    }
+		if (ds() != 0) {
+			dm0 = m0 - m1;
+			m1 = m0;
+		}
 
-    return ds();
-  }
+		return ds();
+	}
 
-  int read() {
-    return push(get());
-  }
+	int read() {
+		return push(get());
+	}
 
-  int write(int s) {
-    return push(set(s));
-  }
+	int write(int s) {
+		return push(set(s));
+	}
 
-  int step() {
-    return mode == OUTPUT ? write(s0) : read();
-  }
+	int step() {
+		return mode == OUTPUT ? write(s0) : read();
+	}
 };
 
 enum port_pin : uint8_t {
-  port_fault_pin     = 2,
-  port_emergency_pin = 4,
-  port_ignition_pin  = 5,
-  port_unlock_pin    = 8,
-  port_lock_pin      = 12,
-  port_battery_pin   = A0
+	port_fault_pin     = 2,
+	port_third_pin     = 3,
+	port_emergency_pin = 4,
+	port_ignition_pin  = 5,
+	port_acc_pin       = 6,
+	port_rpm_pin       = 7,
+
+	port_unlock_pin    = 8,
+	port_lock_pin      = 12,
+
+	port_battery_pin   = A0,
+	port_oxygen_pin    = A1,
+	port_coolant_pin   = A2,
+	port_oil_pin       = A3,
+	port_map_pin       = A4,
+	port_light_pin     = A5,
 };
 
 port ports[] = {
-  port(port_fault_pin    , INPUT_PULLUP             ),
-  port(port_emergency_pin, INPUT_PULLUP             ),
-  port(port_ignition_pin , INPUT_PULLUP             ),
-  port(port_unlock_pin   , OUTPUT      , false, HIGH),
-  port(port_lock_pin     , OUTPUT      , false, HIGH),
-  port(port_battery_pin  , INPUT       , true       ),
+	port(port_fault_pin    , INPUT_PULLUP             ),
+	port(port_third_pin    , INPUT_PULLUP             ),
+	port(port_emergency_pin, INPUT_PULLUP             ),
+	port(port_ignition_pin , INPUT_PULLUP             ),
+	port(port_acc_pin      , INPUT_PULLUP             ),
+	port(port_rpm_pin      , INPUT_PULLUP             ),
+
+	port(port_unlock_pin   , OUTPUT      , false, HIGH),
+	port(port_lock_pin     , OUTPUT      , false, HIGH),
+
+	port(port_battery_pin  , INPUT       , true       ),
+	port(port_oxygen_pin   , INPUT       , true       ),
+	port(port_coolant_pin  , INPUT       , true       ),
+	port(port_oil_pin      , INPUT       , true       ),
+	port(port_map_pin      , INPUT       , true       ),
+	port(port_light_pin    , INPUT       , true       ),
 };
 
 port& port_fault     = ports[0];
-port& port_emergency = ports[1];
-port& port_ignition  = ports[2];
-port& port_unlock    = ports[3];
-port& port_lock      = ports[4];
-port& port_battery   = ports[5];
+port& port_third     = ports[1];
+port& port_emergency = ports[2];
+port& port_ignition  = ports[3];
+port& port_acc       = ports[4];
+port& port_rpm       = ports[5];
+
+port& port_unlock    = ports[6];
+port& port_lock      = ports[7];
+
+port& port_battery   = ports[8];
+port& port_oxygen    = ports[9];
+port& port_coolant   = ports[10];
+port& port_oil       = ports[11];
+port& port_map       = ports[12];
+port& port_light     = ports[13];
 
 const unsigned int ports_n = sizeof(ports) / sizeof(*ports);
 
@@ -213,137 +243,158 @@ const unsigned int ports_n = sizeof(ports) / sizeof(*ports);
 linebuf buf;
 
 void version() {
-  Serial.println("");
-  Serial.println("*************************************");
-  Serial.println("* - El Camino Control System v1.0 - *");
-  Serial.println("* - Copyright(c) 2015, 256 LLC    - *");
-  Serial.println("* - Written by Christopher Abad   - *");
-  Serial.println("*************************************");
-  Serial.println("");
+	Serial.println("");
+	Serial.println("*************************************");
+	Serial.println("* - El Camino Control System v1.0 - *");
+	Serial.println("* - Copyright(c) 2015, 256 LLC    - *");
+	Serial.println("* - Written by Christopher Abad   - *");
+	Serial.println("*************************************");
+	Serial.println("");
 }
 
 void help() {
 
-  Serial.println("");
-  Serial.println("********************");
-  Serial.println("* - Command Help - *");
-  Serial.println("********************");
-  Serial.println("");
-  Serial.println("  help");
-  Serial.println("  version");
-  Serial.println("");
+	Serial.println("");
+	Serial.println("********************");
+	Serial.println("* - Command Help - *");
+	Serial.println("********************");
+	Serial.println("");
+	Serial.println("  help");
+	Serial.println("  version");
+	Serial.println("");
 }
 
 void setup() {
 
-  Serial.begin(115200);
+	Serial.begin(115200);
 
-  delay(500);
+	delay(500);
 
-  version();
+	version();
 }
 
 void buffer_handler() {
 
-  buf.update();
+	buf.update();
 
-  if (buf.is_ready()) {
+	if (buf.is_ready()) {
 
-    if (buf.match("help")) {
+		if (buf.match("help")) {
 
-      help();
+			help();
 
-    } else if (buf.match("version")) {
+		} else if (buf.match("version")) {
 
-      version();
+			version();
 
-    } else {
+		} else {
 
-      bool done = false;
+			bool done = false;
 
-      if (not done) {
-        Serial.print("ERROR: \"");
-        Serial.print(buf.line);
-        Serial.print("\" unknown command");
-        Serial.println("");
-      }
-    }
+			if (not done) {
+				Serial.print("ERROR: \"");
+				Serial.print(buf.line);
+				Serial.print("\" unknown command");
+				Serial.println("");
+			}
+		}
 
-    buf.clear();
-  }
+		buf.clear();
+	}
+}
+
+void port_third_handler(struct port& p) {
+
+	if (p.active and p.ds() != 0)
+		p.status("3rd switch triggered");
+}
+
+void port_rpm_handler(struct port& p) {
+
+	if (p.active and p.ds() != 0)
+		p.status("rpm switch triggered");
+}
+
+void port_acc_handler(struct port& p) {
+
+	if (p.active and p.ds() != 0)
+		p.status("acc switch triggered");
 }
 
 void port_ignition_handler(struct port& p) {
 
-  // deactivate emergency port on ignition rising edge
-  // activate emergency port on ignition falling edge
+	// deactivate emergency port on ignition rising edge
+	// activate emergency port on ignition falling edge
 
-  if (p.active and p.ds() != 0)
-    p.status("ignition switch triggered");
-
+	if (p.active and p.ds() != 0) {
+		p.status("ignition switch triggered");
+	}
 }
 
 void port_fault_handler(port& p) {
 
-  // trigger fault on edge detect
-  // deactivate emergency port on falling edge
-  // activate emergency port on rising edge.
+	// trigger fault on edge detect
 
-  if (p.active and p.ds() != 0)
-    p.status("fault switch level change");
-
+	if (p.active and p.ds() != 0) {
+		p.status("fault switch level change");
+	}
 }
 
 void port_emergency_handler(port& p) {
 
-  // when emergency port is active if level is LOW  for 3000ms, trigger unlock port and deactivate emergency port
-  // activate emergency port on rising edge when inactive
+	// when emergency port is active if level is LOW for 3000ms, trigger unlock port and deactivate emergency port
+	// activate emergency port on rising edge when inactive
 
-  if (p.active) {
+	if (p.active) {
 
-    if (p.s0 == LOW and p.ds() == 0 and p.dm() >= 3000) {
-      p.active = false;
-      p.status("emergency unlock switch triggered");
+		if (p.s0 == LOW and p.ds() == 0 and p.dm() >= 3000) {
+			p.active = false;
+			p.status("emergency unlock switch triggered");
 
-      port_unlock.write(LOW);
-      port_unlock.status();
+			port_unlock.write(LOW);
+			port_unlock.status();
 
-      delay(100);
+			delay(100);
 
-      port_unlock.write(HIGH);
-      port_unlock.status();
+			port_unlock.write(HIGH);
+			port_unlock.status();
 
-      delay(100);
+			delay(100);
 
-      port_unlock.write(LOW);
-      port_unlock.status();
+			port_unlock.write(LOW);
+			port_unlock.status();
 
-      delay(100);
+			delay(100);
 
-      port_unlock.write(HIGH);
-      port_unlock.status();
-    }
+			port_unlock.write(HIGH);
+			port_unlock.status();
+		}
 
-  } else {
+	} else {
 
-    if (p.ds() > 0) {
-      p.active = true;
-      p.status("emergency unlock switch reactivated");
-    }
+		if (p.ds() > 0) {
+			p.active = true;
+			p.status("emergency unlock switch reactivated");
+		}
 
-  }
+	}
 
 }
 
 void port_handler(port& p) {
 
-  p.step();
+	p.step();
 
-  switch (p.pin) {
-    case port_emergency_pin: port_emergency_handler(p); break;
-    case port_ignition_pin : port_ignition_handler (p); break;
-    case port_fault_pin    : port_fault_handler    (p); break;
-  }
+	switch (p.pin) {
+		case port_emergency_pin: port_emergency_handler(p); break;
+		case port_ignition_pin : port_ignition_handler (p); break;
+		case port_acc_pin      : port_acc_handler      (p); break;
+		case port_rpm_pin      : port_rpm_handler      (p); break;
+		case port_fault_pin    : port_fault_handler    (p); break;
+		case port_third_pin    : port_third_handler    (p); break;
+
+
+	}
 
 
 }
@@ -352,10 +403,10 @@ unsigned int idx = 0;
 
 void loop() {
 
-  port_handler(ports[idx]);
-  buffer_handler();
+	port_handler(ports[idx]);
+	buffer_handler();
 
-  ++idx %= ports_n;
+	++idx %= ports_n;
 }
 
 
