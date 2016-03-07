2d10aaf4b7cae92fb5d959636955bcb5805e17af
Critter Harkin <critter.harkin@gmail.com>
2016-03-07 13:48:54 -0800

    .

---
+++ stty.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ ! -c "$1" ]; then
+	echo
+	echo usage: $0 arduino_device
+	echo
+	exit
+fi
+
+stty -F $1 1:0:800018b2:0:3:1c:7f:15:4:5:1:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0
