4dae439d01b0b8a6c0c394139275a76a3e8a2aee
Christopher Abad <aempirei@256.bz>
2016-04-22 08:15:20 -0700

    .

--- stty.sh
+++ stty.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ ! -c "$1" ]; then
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+	prog=`basename "$0"`
 	echo
-	echo usage: $0 arduino_device
+	echo "usage: $prog [OPTION]... [DEVICE]"
 	echo
+	echo "    -h, --help    show this help"
+	echo
+	exit 
+fi
+
+if [ -z "$1" ]; then
+	device="/dev/ttyUSB?"
+else
+	device="$1"
+fi
+
+if [ -c $device ]; then
+	echo configuring device $device
+else
+	echo ERROR: $device is not a valid character device.
 	exit
 fi
 
-stty -F $1 406:0:18b2:8a30:3:1c:7f:8:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0
+config='406:0:18b2:8a30:3:1c:7f:8:4:2:64:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0'
 
-stty -F $1
+stty -F $device "$config"
+stty -F $device
